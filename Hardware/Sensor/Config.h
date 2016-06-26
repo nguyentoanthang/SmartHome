50bd70cde34a7050d89f3933f2064052b1a8bd1f
Thang <nguyentoanthang18@gmail.com>
2016-06-26 17:27:53 +0700

    * Update Sensor/Config.h

--- Hardware/Sensor/Config.h
+++ Hardware/Sensor/Config.h
@@ -1,6 +1,15 @@
 #ifdef _CONFIG_H_
 #define _CONFIG_H_
 
+#include <ESP8266WiFi.h>          
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>
+#include <IRremoteESP8266.h>    
+#include <MQTTClient.h>   
+#include <Ticker.h>
+
+#define DEBUG
 
 
 
